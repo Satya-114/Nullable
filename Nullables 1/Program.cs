ea3de5007c0ce6ddd3312a64b0587c5fd25acc8c
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-26 22:11:16 +0530

    Add files via upload

---
+++ Nullables 1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Nullable_type_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int? a = null;
+            int b = a ?? 3;
+            Console.WriteLine(b);
+        }
+    }
+}
